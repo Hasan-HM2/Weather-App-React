a9793874c8e6a3093deac4128dacebfe481349ab
Hasan-HM2 <hasan21.2000@gmail.com>
2026-03-07 21:10:24 +0300

    get data from API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
 
+// React
+import { useEffect, useState } from 'react'
+
 // Material UI
 import { createTheme, ThemeProvider } from '@mui/material/styles'
 import Container from '@mui/material/Container';
@@ -9,13 +12,54 @@ import Button from '@mui/material/Button';
 // Material Icons
 import CloudIcon from '@mui/icons-material/Cloud';
 
+// Libreries
+import axios from 'axios';
+
 const theme = createTheme({
   typography: {
     fontFamily: ["IBM"]
   }
 });
 
+let cancelAxios = null
+
 function App() {
+  const [temp, setTemp] = useState(null)
+  const [maxTemp, setMaxTemp] = useState(null)
+  const [minTemp, setMinTemp] = useState(null)
+  const [description, setDescription] = useState("")
+
+  useEffect(() => {
+    axios.get('https://api.openweathermap.org/data/2.5/weather?lat=35.29&lon=36.04&appid=84a13e10c6abc34071a954d4e4dd06db', 
+      {
+        cancelToken: new axios.CancelToken((c)=> {
+          cancelAxios = c
+        })
+      }
+    )
+      .then((response) => {
+        const responseTemp = Math.round(response.data.main.temp - 272.15)
+        setTemp(responseTemp)
+
+        const responseMaxTemp = Math.round(response.data.main.temp_max - 272.15)
+        setMaxTemp(responseMaxTemp)
+
+        const responseMinTemp = Math.round(response.data.main.temp_min - 272.15)
+        setMinTemp(responseMinTemp)
+
+        const responseDescription = response.data.weather[0].description
+        setDescription(responseDescription)
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+
+    return () => {
+      cancelAxios()
+    }
+  }, [])
+
+
   return (
     <div className="App">
       <ThemeProvider theme={theme}>
@@ -52,17 +96,17 @@ function App() {
                   {/* TEMPRETURE */}
                   <div>
                     <Typography variant="h1" style={{ textAlign: 'right' }}>
-                      16
+                      {temp}
                     </Typography>
                     {/* TODO: TEMP IMAGE */}
 
                     {/* === TEMPRETURE ==== */}
                     <Typography variant="h5" style={{ textAlign: 'right' }}>
-                      غائم جزئياً
+                      {description}
                     </Typography>
 
                     <Typography variant="h7" style={{ textAlign: 'right' }}>
-                      الصغرى: 10 | الكبرى: 16
+                      الصغرى: {minTemp} | الكبرى: {maxTemp}
                     </Typography>
                   </div>
 
